c86fba63eb87ddcf50b33f7d497e61e194832d94
Chamo Escalona <rolando.escalona@gmail.com>
2024-07-09 07:58:24 -0600

    Creating small FrontEnd Masters project

---
+++ src/Carousel.jsx
@@ -0,0 +1,41 @@
+import { Component } from "react";
+
+export default class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["https://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="Animale Hero" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              alt="Animal Thumbnail"
+              key={photo}
+              className={index == active ? "active" : ""}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/SearchParams.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect, useContext } from "react";
+import useBreedList from "./useBreedList";
+import Results from "./Results";
+import AdoptedPetContext from "./AdoptedPetContext";
+const ANIMALS = ["cat", "dog", "bird", "reptile", "rabbit"];
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+  const [adoptedPet] = useContext(AdoptedPetContext);
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={breeds.length == 0}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
